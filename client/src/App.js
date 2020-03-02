606eab7a34f76d0c137f52023de52d60183184ef
unknown <irisjitomo@yahoo.com>
2020-03-02 11:27:31 -0500

    Register component

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Register from './components/register/Register';
 function App() {
   return (
     <div className="App">
-      <Router path="/register" component={Register} />
+      <Route path="/register" component={Register} />
     </div>
   );
 }
