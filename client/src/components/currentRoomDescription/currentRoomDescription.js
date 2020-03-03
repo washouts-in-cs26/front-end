c69840cdd9d4d5b15fbff34792d39275bc5c5665
unknown <irisjitomo@yahoo.com>
2020-03-03 12:51:56 -0500

    made reducers file, currentRoomDesciption.js created

---
+++ client/src/components/currentRoomDescription/currentRoomDescription.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const currentRoomDescription = () => {
+     return (
+         <div>
+             <h1>title (get initInfo to display here)</h1>
+             <h2>description</h2>
+             <h3>Players with you: //get players from initInfo.players</h3>
+         </div>
+     )
+}
+
+export default currentRoomDescription
