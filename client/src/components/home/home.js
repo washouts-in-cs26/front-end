8e32abde0cbc21ecb631c3018c58567a5e9b9bd0
Taran <thetaranneeld@gmail.com>
2020-03-03 16:32:34 -0700

    added gif to home page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './home.css'
-import frame1 from '../../images/chest_intro_closed.png'
-import frame2 from '../../images/chest_intro_half.png'
-import frame3 from '../../images/chest_intro_full.png'
+
+
 const Home = () => {
-    const images = [
-        frame1,
-        frame2,
-        frame3
-    ]
+ 
     return (
         <div>
-            {images.map(image => {
-               return <img src={image}/>
-            })}
+            <img src="https://media.giphy.com/media/ehaU0nwrUjlprO0uKg/giphy.gif"></img>
         </div>
     )
 }
