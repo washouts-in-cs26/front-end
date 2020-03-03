ad0b8d0612667c4f19f7f4bae4409b4463d28090
unknown <irisjitomo@yahoo.com>
2020-03-03 17:30:29 -0500

    added actions index file, just need those endpoints

---
+++ client/src/store/actions/index.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
+
+export const FETCH_DATA_START = "FETCH_DATA_START"
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS"
+export const FETCH_DATA_FAILURE = "FETCH_TRAILS_FAILURE"
+
+export const getData = () => {
+	return (dispatch) => {
+		dispatch({ type: FETCH_DATA_START });
+		axios
+			.get('URL HERE') // add /map endpoint
+			.then((res) => {
+                const roomData = res.data
+                dispatch({ type: FETCH_DATA_SUCCESS, payload: roomData})
+            })
+            .catch(err => {
+                dispatch({ type: FETCH_DATA_FAILURE, payload: `${err}` })
+                console.log('fetching data failure', err)
+            })
+	};
+};
+
+export const FETCH_INIT_START = "FETCH_INIT_START"
+export const FETCH_INIT_SUCCESS = "FETCH_INIT_SUCCESS"
+export const FETCH_INIT_FAILURE = "FETCH_INIT_FAILURE"
+
+export const getInit = () => dispatch => {
+    dispatch({type: FETCH_INIT_START})
+    axiosWithAuth().get("URL HERE") // add /init endpoint
+        .then(response => {
+            dispatch({type: FETCH_INIT_SUCCESS, payload: response.data})
+            console.log("getInit response", response)
+        })
+        .catch(err => {
+            dispatch({type: FETCH_INIT_FAILURE, payload: `${err}`})
+            console.log("errrrrrror from getInit", err)
+        })
+}
